fa4bde8fafa4e9e938a6bc7354ea00d808a3b5c2
Miles Blackwood <milesblackwoodmusic@gmail.com>
2017-03-27 20:53:45 -0400

    sentiment.

--- build.rb
+++ build.rb
@@ -19,16 +19,24 @@ end
 
 data = source
 
-data["shows"] = source["shows"].each do |show| 
+# Sweeten date info before creating 'data' variables for HTML template.
+# Add a property "future" to indicate next gig.
+data["history"] = source["history"].each do |show| 
   show["month"] = Date::MONTHNAMES[show["date"].month][0..2].upcase
   show["future"] = show["date"] > Date.today
   show
 end
 
+booked = data["history"].index{ |s| s["future"] }
+upcoming = (booked ? booked : data["history"].length) - 1 
+
+data["upcoming"] = data["history"].slice(upcoming, 3)
+data["timesigs"] = ["last", "next", "future"]
+
 template = ERB.new File.read("src/index.html.erb"), nil, "%"
 
 File.open("dist/index.html", "w") { |file|
-  file.write(template.result(DataBinding.new(data).get_binding)) 
+  file.write(template.result(DataBinding.new(data).get_binding))
 }
 
 File.write("dist/main.css", CSSminify.compress(File.open("src/main.css")))
