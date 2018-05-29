96b1ed759489b09565df4ea4816cf7275ac216ab
Miles Robinson <milesblackwoodmusic@gmail.com>
2018-05-28 21:49:04 -0400

    Initial sheets work.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,32 @@
+// require("dotenv").config({
+//   path: `.env`
+// });
+// console.log('IN CONFIG', process.env.NODE_ENV);
+
 module.exports = {
-	siteMetadata: {
-		title: `Gatsby Default Starter`
-	},
-	plugins: [`gatsby-plugin-react-helmet`]
+  siteMetadata: {
+    title: `Gatsby Default Starter`
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    // {
+    //   resolve: "gatsby-source-google-sheets",
+    //   options: {
+    //     spreadsheetId: process.env.GSHEET_SPREADSHEET_ID,
+    //     worksheetTitle: process.env.GSHEET_WORKSHEET_TITLE,
+    //     credentials: {
+    //       type: process.env.type,
+    //       project_id: process.env.project_id,
+    //       private_key_id: process.env.private_key_id,
+    //       private_key: process.env.private_key,
+    //       client_email: process.env.client_email,
+    //       client_id: process.env.client_id,
+    //       auth_uri: process.env.auth_uri,
+    //       token_uri: process.env.token_uri,
+    //       auth_provider_x509_cert_url: process.env.auth_provider_x509_cert_url,
+    //       client_x509_cert_url: process.env.client_x509_cert_url
+    //     }
+    //   }
+    // }
+  ]
 };
--- gatsby-node.js
+++ gatsby-node.js
@@ -1,3 +1,124 @@
-const Promise = require("bluebird");
-const { createFilePath } = require(`gatsby-source-filesystem`);
+// const Promise = require("bluebird");
+// const { createFilePath } = require(`gatsby-source-filesystem`);
 
+// exports.onCreateNode = ({ node, getNode, boundActionCreators }) => {
+//   const { createNodeField } = boundActionCreators;
+//   console.log(node.internal.type);
+//   if (node.internal.type === `googleSheetMainRow`) {
+//     const slug = createFilePath({ node, getNode, basePath: `pages` });
+//     createNodeField({
+//       node,
+//       name: `slug`,
+//       value: slug
+//     });
+//   }
+// };
+
+// exports.createPages = ({ graphql, boundActionCreators }) => {
+//   const { createPage } = boundActionCreators;
+//   console.log("IN NODE", process.env.NODE_ENV);
+//   return new Promise((resolve, reject) => {
+//     graphql(
+//       `
+//         {
+//           allGoogleSheetMainRow {
+//             edges {
+//               node {
+//                 id
+//                 name
+//                 val
+//                 goodJob
+//               }
+//             }
+//           }
+//         }
+//       `
+//     ).then(result => {
+//       if (result.errors) {
+//         reject(result.errors);
+//       }
+//       result.data.allGoogleSheetMainRow.edges.forEach(({ node }) => {
+//         createPage({
+//           path: node.fields.slug,
+//           component: path.resolve(`./src/layouts/index.js`),
+//           context: {
+//             // Data passed to context is available in page queries as GraphQL variables.
+//             slug: node.fields.slug
+//           }
+//         });
+//       });
+//       resolve();
+//     });
+//   });
+// };
+// // can get working from this example?
+// new Promise()
+//   .then(result => {
+//     console.log(result);
+//     if (result.errors) {
+//       reject(result.errors);
+//     }
+
+//     // Create Product pages
+//     // We want to create a detailed page for each
+//     // product node. We'll just use the Contentful id for the slug.
+//     _.each(result.data.allContentfulProduct.edges, edge => {
+//       // Gatsby uses Redux to manage its internal state.
+//       // Plugins and sites can use functions like "createPage"
+//       // to interact with Gatsby.
+//       createPage({
+//         // Each page is required to have a `path` as well
+//         // as a template component. The `context` is
+//         // optional but is often necessary so the template
+//         // can query data specific to each page.
+//         path: `/products/${edge.node.id}/`,
+//         component: slash(productTemplate),
+//         context: {
+//           id: edge.node.id
+//         }
+//       });
+//     });
+//     resolve();
+//   })
+//   .then(() => {
+//     graphql(
+//       `
+//         {
+//           allContentfulCategory(limit: 1000) {
+//             edges {
+//               node {
+//                 id
+//               }
+//             }
+//           }
+//         }
+//       `
+//     ).then(result => {
+//       if (result.errors) {
+//         reject(result.errors);
+//       }
+
+//       // Create Category pages
+//       const categoryTemplate = path.resolve(`./src/templates/category.js`);
+//       // We want to create a detailed page for each
+//       // category node. We'll just use the Contentful id for the slug.
+//       _.each(result.data.allContentfulCategory.edges, edge => {
+//         // Gatsby uses Redux to manage its internal state.
+//         // Plugins and sites can use functions like "createPage"
+//         // to interact with Gatsby.
+//         createPage({
+//           // Each page is required to have a `path` as well
+//           // as a template component. The `context` is
+//           // optional but is often necessary so the template
+//           // can query data specific to each page.
+//           path: `/categories/${edge.node.id}/`,
+//           component: slash(categoryTemplate),
+//           context: {
+//             id: edge.node.id
+//           }
+//         });
+//       });
+
+//       resolve();
+//     });
+//   });
